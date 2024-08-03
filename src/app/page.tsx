d682bb03c1ae99be4e24d5ff383e38e96e9814c0
Omkar Date <omkardate29@gmail.com>
2024-08-03 21:17:59 +0530

    fix: landing title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <h1 className='text-center text-3xl font-bold'>Miniel</h1>
+      <div className='text-center'>
+        <h1 className='-mb-1 text-3xl font-bold'>Miniel</h1>
+        <p className='text-sm font-medium text-gray-400'>A URL Shortner</p>
+      </div>
     </main>
   );
 }
