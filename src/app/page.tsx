466fa0636618342bdfdc4ce3696a181a2e41a6c7
Omkar Date <omkardate29@gmail.com>
2024-08-03 20:59:58 +0530

    chore: project name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className='text-center text-3xl font-bold'>Shortly</h1>
+      <h1 className='text-center text-3xl font-bold'>Miniel</h1>
     </main>
   );
 }
